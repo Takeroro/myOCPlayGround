70714d9a64cd1bebceebe4d567c1f9364d416ca9
Caotingjun <Caotingjun@terrencedemacbook-pro.local>
2016-10-21 16:15:49 +0800

    Initial Commit

---
+++ PlayWithNSCopy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PlayWithNSCopy
+//
+//  Created by Caotingjun on 16/10/21.
+//  Copyright © 2016年 Caotingjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
